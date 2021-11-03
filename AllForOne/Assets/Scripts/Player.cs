52c18798e68fc635d17b1125c06c9d58ab0117ed
Kamil <kamil2002molekx@gmail.com>
2021-11-03 16:28:45 +0100

    [TASK] made a unit maker

--- AllForOne/Assets/Scripts/Player.cs
+++ AllForOne/Assets/Scripts/Player.cs
@@ -6,9 +6,8 @@ using TMPro;
 
 public class Player : MonoBehaviour
 {
-
+    //set the money of the each player
     public int Money = 100;
-
     public TMP_Text MoneyText;
     
     void Start()
@@ -17,9 +16,12 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-
+        //connet the int to the ui
         MoneyText.text = this.gameObject.name.ToString()+ " money: " + Money.ToString();
     }
+    
+    
+    //instatiet unit
     
     
 }
